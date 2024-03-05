8b5986f71b26ed38f0db7b8ea967036ad9adf5cf
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2024-03-05 19:34:26 -0400

    feat: creation of context hooks to provide the data for the screen containers, by invoking the useUser function

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -7,6 +7,7 @@ import * as Yup from 'yup'
 import LoginBanner from '../../assets/login-banner.svg'
 import Logo from '../../assets/logo.svg'
 import Button from '../../components/Button'
+import { useUser } from '../../hooks/UserContext'
 import api from '../../services/api'
 import {
   Container,
@@ -19,6 +20,9 @@ import {
 } from './styles'
 
 function Login() {
+  const users = useUser()
+  console.log(users)
+
   const schema = Yup.object().shape({
     email: Yup.string()
       .email('Digite um e-mail válido')
---
+++ src/hooks/UserContext.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types'
+import React, { createContext, useContext } from 'react'
+
+const UserContext = createContext({})
+// context structure:
+export const UserProvider = ({ children }) => {
+  // this example is the data I want to save and make available
+  const user = { name: 'lucas', age: 23 }
+  const outroUser = { name: 'levi', age: 20 }
+
+  return (
+    <UserContext.Provider value={[user, outroUser]}>
+      {children}
+    </UserContext.Provider>
+  )
+}
+// provide the data for the screen containers by invoking the useUser function
+export const useUser = () => {
+  const context = useContext(UserContext)
+  // here only if, by some mistake, we invoke useUser and it is empty, it tells the console that the problem is here.
+  if (!context) {
+    throw new Error('useUser most be used with UserContext')
+  }
+  return context
+}
+
+UserProvider.propTypes = {
+  children: PropTypes.node
+}
