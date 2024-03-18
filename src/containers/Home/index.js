f75324962bffd65953c64242cfec1ccf3c4d6c29
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2024-03-18 04:40:25 -0400

    feat: add screem home and new components carousel

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import BannerHome from '../../assets/burger-home.svg'
+import CategoryCarousel from '../../components/CategoryCarousel'
 import { Conteiner, HomeImage } from './styles'
 
 function Home() {
   return (
     <Conteiner>
       <HomeImage src={BannerHome} alt="home-image" />
+      <CategoryCarousel />
     </Conteiner>
   )
 }
