8f28ff7f46599ec31a3c478a9df67b01483b5597
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2024-03-28 02:11:50 -0400

    feat: add carousel

--- src/components/CategoryCarousel/index.js
+++ src/components/CategoryCarousel/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import Carousel from 'react-elastic-carousel'
 
 import LabelCategory from '../../assets/label-categorias.png'
 import api from '../../services/api'
@@ -16,10 +17,20 @@ function CategoryCarousel() {
 
     loadCategories()
   }, [])
+  // console.log(categories.categories[1].url)
 
   return (
     <Conteiner>
       <CategoryImage src={LabelCategory} alt="logo-category-image" />
+      <Carousel itemsToShow={5}>
+        {Array.isArray(categories.categories) &&
+          categories.categories.map((category) => (
+            <div key={category.id}>
+              <img src={category.url} alt="foto da categoria" />
+              <button>{category.name}</button>
+            </div>
+          ))}
+      </Carousel>
     </Conteiner>
   )
 }
