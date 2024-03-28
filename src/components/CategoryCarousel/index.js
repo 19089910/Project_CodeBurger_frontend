7172a13edc60a1a4a6bd0f4c8ee579b659bcf927
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2024-03-28 02:27:50 -0400

    feat: style carousel

--- src/components/CategoryCarousel/index.js
+++ src/components/CategoryCarousel/index.js
@@ -3,7 +3,13 @@ import Carousel from 'react-elastic-carousel'
 
 import LabelCategory from '../../assets/label-categorias.png'
 import api from '../../services/api'
-import { Conteiner, CategoryImage } from './styles'
+import {
+  Conteiner,
+  CategoryImage,
+  ConteinerItems,
+  Image,
+  Button
+} from './styles'
 
 function CategoryCarousel() {
   const [categories, setCategories] = useState([])
@@ -17,18 +23,29 @@ function CategoryCarousel() {
 
     loadCategories()
   }, [])
-  // console.log(categories.categories[1].url)
+
+  const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 400, itemsToShow: 2 },
+    { width: 600, itemsToShow: 3 },
+    { width: 900, itemsToShow: 4 },
+    { width: 1300, itemsToShow: 5 }
+  ]
 
   return (
     <Conteiner>
       <CategoryImage src={LabelCategory} alt="logo-category-image" />
-      <Carousel itemsToShow={5}>
+      <Carousel
+        itemsToShow={5}
+        styles={{ width: '90%' }}
+        breakPoints={breakPoints}
+      >
         {Array.isArray(categories.categories) &&
           categories.categories.map((category) => (
-            <div key={category.id}>
-              <img src={category.url} alt="foto da categoria" />
-              <button>{category.name}</button>
-            </div>
+            <ConteinerItems key={category.id}>
+              <Image src={category.url} alt="foto da categoria" />
+              <Button>{category.name}</Button>
+            </ConteinerItems>
           ))}
       </Carousel>
     </Conteiner>
--- src/components/CategoryCarousel/styles.js
+++ src/components/CategoryCarousel/styles.js
@@ -1,5 +1,47 @@
 import styled from 'styled-components'
 
-export const Conteiner = styled.div``
+export const Conteiner = styled.div`
+  background-color: #efefef;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 35px;
+  padding: 35px 0;
+`
 
 export const CategoryImage = styled.img``
+
+export const ConteinerItems = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const Image = styled.img`
+  width: 200px;
+  border-radius: 10px;
+`
+
+export const Button = styled.button`
+  margin-top: 16px;
+  background: #9758a6;
+  border-radius: 8px;
+
+  height: 50px;
+  border: none;
+
+  font-style: normal;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 100%;
+  text-align: center;
+  color: #ffffff;
+
+  cursor: poiter;
+  &:hover {
+    opacity: 0.8;
+  }
+
+  &active {
+    opacity: 0.6;
+  }
+`
