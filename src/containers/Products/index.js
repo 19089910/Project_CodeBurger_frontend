7bf73b9203087f82e598071e00cdb30a54b9d7ec
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2024-04-03 21:30:20 -0400

    feat: add Category menu

--- src/containers/Products/index.js
+++ src/containers/Products/index.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import BannerProduct from '../../assets/banner-product.svg'
+import api from '../../services/api'
 import { Conteiner, ProductImage } from './styles'
-
 function Products() {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    async function loadCategories() {
+      const { data } = await api.get('/categories')
+
+      setCategories(data)
+    }
+
+    loadCategories()
+  }, [])
+
   return (
     <Conteiner>
       <ProductImage src={BannerProduct} alt="product-image" />
+      {Array.isArray(categories.categories) &&
+        categories.categories.map((category) => (
+          <button key={category.id}>{category.name}</button>
+        ))}
     </Conteiner>
   )
 }
