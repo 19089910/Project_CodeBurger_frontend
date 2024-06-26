76524a0ca9dd60fcef6cdba8531a899b2f355b0f
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2024-06-25 22:29:55 -0400

    fix: fixing the information of carResume

--- src/components/CartResume/index.js
+++ src/components/CartResume/index.js
@@ -6,7 +6,7 @@ import { useCart } from './../../hooks/CartContext'
 import { Conteiner } from './styles'
 
 export function CartResume() {
-  const [finalPrice, setFinalPrice] = useState(0)
+  const [itemsPrice, setItemsPrice] = useState(0)
   const [deliveryTax] = useState(5)
 
   const { cartProducts } = useCart()
@@ -15,8 +15,8 @@ export function CartResume() {
     const sumAllItems = cartProducts.reduce((acc, current) => {
       return current.price * current.quantity + acc
     }, 0)
-    setFinalPrice(formatCurrency(sumAllItems + deliveryTax))
-  }, [cartProducts, deliveryTax])
+    setItemsPrice(sumAllItems)
+  }, [cartProducts])
 
   return (
     <div>
@@ -24,13 +24,13 @@ export function CartResume() {
         <div className="container-top">
           <h2 className="title">resumo do pedido</h2>
           <p className="items">Items</p>
-          <p className="items-price">R$ 10,00</p>
+          <p className="items-price">{formatCurrency(itemsPrice)}</p>
           <p className="delivery-tax">Taxa de entrega</p>
           <p className="delivery-tax-price">{formatCurrency(deliveryTax)}</p>
         </div>
         <div className="container-bottom">
           <p>Total</p>
-          <p>R$ 20,00</p>
+          <p>{formatCurrency(itemsPrice + deliveryTax)}</p>
         </div>
       </Conteiner>
       <Button style={{ width: '100%', marginTop: 30 }}>Finalizar pedido</Button>
