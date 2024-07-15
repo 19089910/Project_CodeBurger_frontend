a222d8510a2be8d1df4ca190075e8ec2883c0950
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2024-07-14 23:53:41 -0400

    fiat: added the requirement to go to the cart by product

--- src/components/CardProduct/index.js
+++ src/components/CardProduct/index.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import { useCart } from '../../hooks/CartContext'
 import { Button } from '../Button'
@@ -7,13 +8,22 @@ import { Conteiner, Image, ProductName, ProductPrice } from './styles'
 
 export function CardProduct({ product }) {
   const { putProductInCart } = useCart()
+  const navigate = useNavigate()
+
   return (
     <Conteiner>
       <Image src={product.url} alt="imagem-do_produto" />
       <div>
         <ProductName>{product.name}</ProductName>
         <ProductPrice>{product.formatedPrice}</ProductPrice>
-        <Button onClick={() => putProductInCart(product)}>Adicionar</Button>
+        <Button
+          onClick={() => {
+            putProductInCart(product)
+            navigate('/carrinho')
+          }}
+        >
+          Adicionar
+        </Button>
       </div>
     </Conteiner>
   )
